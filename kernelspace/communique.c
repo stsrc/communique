8f81eb356b9847af9e36b51f9b904d3ebe0a9ed5
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2015-11-09 23:03:36 +0100

    rb tree implementation

--- kernelspace/communique.c
+++ kernelspace/communique.c
@@ -7,16 +7,15 @@
 #include <asm/uaccess.h>
 #include <linux/module.h>
 #include <linux/cdev.h>
-#include <linux/slab.h> 
 #include <linux/device.h>
 #include <linux/err.h>
-#include <linux/keyboard.h>
-#include <linux/reboot.h>
 #include <linux/moduleparam.h>
 #include <linux/stat.h>
-
 #include <linux/kernel.h>
+#include <linux/slab.h>
 #include <uapi/asm-generic/ioctl.h>
+#include <linux/rbtree.h>
+
 
 MODULE_LICENSE("GPL");
 
@@ -27,9 +26,9 @@ MODULE_LICENSE("GPL");
 #define debug_message()
 #endif
 
-#define SETEVENT _IOW('|', 0x70, char __user *)
-#define WAITFOREVENT _IOW('|', 0x71, char __user *)
-#define THROWEVENT _IOW('|', 0x72, char __user *)
+#define SETEVENT _IOW(0x8A, 0x01, char __user *)
+#define WAITFOREVENT _IOW(0x8A, 0x02, char __user *)
+#define THROWEVENT _IOW(0x8A, 0x03, char __user *)
 
 struct communique {
 	const char *name;
@@ -38,73 +37,135 @@ struct communique {
 	struct class *class;
 	struct file_operations fops;
 	struct device *device;
+	struct rb_root proc_rel_tree;
 };
 
-struct process_relation {
+struct proc_rel {
 	pid_t pid;
-	uint8_t throws;
-	uint8_t reacts;
+	uint8_t event_to_throw;
 	uint8_t sleeps;
+	struct rb_node node;
 };
+
+/*
+ * functions implemented specially for rbtree
+ */
+struct proc_rel *communique_rb_search(struct rb_root *root, int event)
+{
+	struct rb_node *node = root->rb_node;
+	while (node) {
+		struct proc_rel *data = rb_entry(node, struct proc_rel, node);
+		if (event < data->event_to_throw)
+			node = node->rb_left;
+		else if (event > data->event_to_throw)
+			node = node->rb_right;
+		else
+			return data;
+	}
+	return NULL;
+}
+
+int communique_rb_insert(struct rb_root *root, struct proc_rel *data)
+{
+	struct rb_node **new = &(root->rb_node), *parent = NULL;
+	while (*new) {
+		struct proc_rel *this = rb_entry(*new, struct proc_rel, node);
+		parent = *new;
+		if (data->event_to_throw < this->event_to_throw)
+			new = &((*new)->rb_left);
+		else if (data->event_to_throw > this->event_to_throw)
+			new = &((*new)->rb_right);
+		else
+			return -EINVAL;
+	}
+	rb_link_node(&data->node, parent, new);
+	rb_insert_color(&data->node, root);
+	return 0;
+}
+
+/*
+ * drivers functions
+ */
 
 int communique_release(struct inode *inode_s, struct file *file_s)
 {
-	debug_message();
 	return 0;
 }
 
 
 int communique_open(struct inode *inode_s, struct file *file_s)
 {
-	debug_message();
 	return 0;
 }
 
-int communique_cp_prs(int *to, const char __user *from, uint8_t n)
+static inline int communique_cp_prs(int *to, const char __user *from, uint8_t n)
 {
-	debug_message();
-
 	int rt;
-	char temp[3 * sizeof(int)];
-	if (n > sizeof(temp)) 
-		return -EFAULT;
-	rt = copy_from_user(temp, from, n);
+	rt = copy_from_user((char *)to, from, n * sizeof(int));
 	if (rt)
 		return -EAGAIN;
-	sscanf(temp, "%d%d%d", to, (to + 4), (to + 8));
-	
-	printk(KERN_EMERG "DEBUG: %d, %d, %d\n", to[0], to[1], to[2]);
-
 	return 0;
 }
+
+static struct communique cmc;
 
 int communique_register(const char __user *args)
 {
-	debug_message();
 	int rt = 0;
-	int data[3];
-	rt = communique_cp_prs(data, args, 3);
+	int data[2];
+	struct proc_rel *temp = NULL;
+	rt = communique_cp_prs(data, args, 2);
+	if (rt)
+		return rt;
+	temp = kmalloc(sizeof(struct proc_rel), GFP_KERNEL);
+	if (temp == NULL)
+		return -ENOMEM;
+	temp->pid = data[0];
+	temp->event_to_throw = data[1];
+	communique_rb_insert(&cmc.proc_rel_tree, temp);
 	return 0;
+}
+
+void debug_pr(struct proc_rel *temp)
+{
+	printk(KERN_EMERG "pid: %d, event: %d\n", temp->pid, 
+	       temp->event_to_throw);
 }
 
 int communique_sleep(const char __user *args)
 {
-	debug_message();
-	return -EDEADLK;
+	struct proc_rel *temp = NULL;
+	int rt;
+	int event;
+	rt = communique_cp_prs(&event, args, 1);
+	if (rt)
+		return rt;
+	temp = communique_rb_search(&cmc.proc_rel_tree, event);
+	if (temp == NULL)
+		return -EINVAL;
+	else
+		debug_pr(temp);
+	return 0;
 }
 
 int communique_throw(const char __user *args)
 {
-	debug_message();
-	return 1;
+	struct proc_rel *temp = NULL;
+	int rt;
+	int data[2];
+	rt = communique_cp_prs(data, args, 2);
+	if (rt)
+		return rt;
+	temp = communique_rb_search(&cmc.proc_rel_tree, data[1]);
+	if ((data[0] != temp->pid) || (temp == NULL))
+		return -EACCES;
+	else
+		debug_pr(temp);
+	return 0;
 }
 
 long communique_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-	debug_message();
-	printk(KERN_EMERG "SETEVENT: %lu, WAITFOREVENT: %lu, THROWEVENT: %lu\n"
-	       , SETEVENT, WAITFOREVENT, THROWEVENT);
-	printk(KERN_EMERG "command: %lu\n", (unsigned long)cmd);
 	switch(cmd) {
 	case SETEVENT:
 		return communique_register((char __user *)arg);
@@ -128,7 +189,8 @@ static struct communique cmc = {
 		 .unlocked_ioctl = communique_ioctl,
 		 .compat_ioctl = communique_ioctl
 		},
-	.device = NULL
+	.device = NULL,
+	.proc_rel_tree = RB_ROOT 
 };
 
 static int __init communique_init(void)
